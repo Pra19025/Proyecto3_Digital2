4a1d8efa468f3da8f9919595482be214e1b381e8
mun19508 <68123854+mun19508@users.noreply.github.com>
2021-04-19 15:19:12 -0600

    V3

--- main/main.ino
+++ main/main.ino
@@ -42,10 +42,17 @@ int poty1 = 122;
 int potx2 = 122;
 int poty2 = 122;
 
+/*int j1_pos_prev;
+int j1_pos_new;
+int j2_pos_prev;
+int j2_pos_new;*/
 
 int x = 0;
 int y = 0;
-
+int x_j1 = 0;
+int y_j1  = 0;
+int x_j2 = 0;
+int y_j2  = 0;
 
 int anim2;
 //***************************************************************************************************************************************
@@ -62,6 +69,7 @@ void setup() {
   GPIOPadConfigSet(GPIO_PORTB_BASE, 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7, GPIO_STRENGTH_8MA, GPIO_PIN_TYPE_STD_WPU);
   Serial.println("Inicio");
   LCD_Init();
+<<<<<<< Updated upstream
   LCD_Clear(0x0);  //es el azul
   LCD_Clear(0x0528);
   for (int i = 0; i < 320 - 30; i = i + 32) {
@@ -73,6 +81,15 @@ void setup() {
   for (int i = 0; i < 320 - 30; i = i + 32) {
     LCD_Bitmap(i,  0, 31, 24, cloud);
   }
+=======
+  LCD_Clear(0x3CDB17);  //es el verde
+  for (int i = 0; i < 320 - 30; i++) {
+    LCD_Bitmap(i,  207, 32, 32, piedras);
+  }
+  FillRect(0, 0, 319, 206, 0x421b);
+  LCD_Bitmap(100,  0, 31, 24, cloud);
+  LCD_Bitmap(250,  0, 31, 24, cloud);
+>>>>>>> Stashed changes
 
   String text1 = "Pelea violenta";
   LCD_Print(text1, 20, 100, 2, 0xffff , 0x0528);
@@ -83,7 +100,6 @@ void setup() {
 // Loop Infinito
 //***************************************************************************************************************************************
 void loop() {
-
 
   // read the analog in value:
   sensorValue1 = analogRead(analogInPin1);
@@ -96,7 +112,6 @@ void loop() {
   poty1 = map(sensorValue2, 0, 4095, 0, 255);
 
 
-
   Serial.print("output X = ");
   Serial.print(potx1);
 
@@ -104,23 +119,43 @@ void loop() {
   Serial.println(poty1);
   delay(10);
 
-
   //codigo para controlar la posición según joystick
-  //la dimensión de la pantalla es: 320*240 pixeles
+  //la dimensión de la pantalla es: 320x240 pixeles
 
-  if (potx1 <= 100) {
-    x--;
-    if (x < 0) {
-      x = 0;
+  anim2 = (potx1) % 3;
+  
+  Mov_x(33);
+  Mov_y(33);
+  
+  LCD_Sprite(x_j1, y_j1, 30, 33, tiburonS, 3, anim2, 0, 0 );
+  
+  V_line(x_j1 - 1, y_j1, y_j1 + 30, 0x421b);
+  LCD_Sprite( x_j1, y_j1 + 33, 32, 32, megamanRojoS, 4, anim2, 0, 0 );
+  V_line( x_j1 - 1, y_j1, y_j1 + 30, 0x421b);
+  delay(15);
+}
+
+void Mov_x(int w) {
+  if (potx <= 100) {
+    aux--;
+    if (potx < 0) {
+      aux = 0;
     }
+<<<<<<< Updated upstream
 
   }
   if (potx1 >= 155) {
     x++;
+=======
   }
-  if (x > 320) {
-    x = 320;
+  if (potx >= 155) {
+    aux++;
+>>>>>>> Stashed changes
   }
+  if (potx > 320 - w) {
+    aux = 320;
+  }
+<<<<<<< Updated upstream
 
   if (poty1 <= 100) {
     y++;
@@ -141,4 +176,21 @@ void loop() {
   // LCD_Sprite(x,y, 30, 33, tiburonS, 3, anim2, 0, 0 );
   LCD_Sprite(x, y, 32, 33, ataqueMega, 3, anim2, 0, 0 );
   delay(15);
+=======
+}
+void Mov_y(int w) {
+  if (poty <= 100) {
+    auy++;
+  }
+  if (poty > 240 - 32 - w) {
+    auy = 240;
+  }
+
+  if (155 <= poty) {
+    auy--;
+  }
+  if (poty < 0) {
+    auy = 0;
+  }
+>>>>>>> Stashed changes
 }
