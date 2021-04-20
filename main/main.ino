24d4682d25805e3b38215ce167fa7699ce891ff0
Noel Prado <68123083+Pra19025@users.noreply.github.com>
2021-04-20 08:59:35 -0600

    ya funcionan los dos joysticks

--- main/main.ino
+++ main/main.ino
@@ -38,8 +38,12 @@ int poty2 = 122;
 int x = 0;
 int y = 0;
 
-String datos = "";
+int x2 = 0;
+int y2 = 0;
 
+
+String datos = "";
+int anim1;
 int anim2;
 //***************************************************************************************************************************************
 // Inicialización
@@ -89,28 +93,27 @@ void loop() {
     }
 
     else {
-      Serial.println(datos);
-     
-      potx1 = (datos.substring(0, 3)).toInt();
-      poty1 = (datos.substring(4, 7)).toInt();
-      potx2 = (datos.substring(8, 11)).toInt();
-      poty2 = (datos.substring(12, 15)).toInt();
-    
-      //Serial.println((datos.substring(4, 7)));
+      //Serial.println(datos);
+  
+      String p1 = getValue(datos, ',', 0);
+      String p2 = getValue(datos, ',', 1);
+      String p3 = getValue(datos, ',', 2);
+      String p4 = getValue(datos, ',', 3);
       
+      potx1 = p1.toInt();     
+      poty1 = p2.toInt();
+      potx2 = p3.toInt();
+      poty2 = p4.toInt();
+
       datos = "";
     }
   }
 
 
 
-
- 
-
-
   //codigo para controlar la posición según joystick
   //la dimensión de la pantalla es: 320*240 pixeles
-
+//PRIMER JOYSTICK
   if (potx1 <= 100) {
     x--;
     if (x < 0) {
@@ -139,9 +142,60 @@ void loop() {
     y = 0;
   }
 
-  anim2 = (x) % 3;
+  anim1 = (x) % 3;
 
-  // LCD_Sprite(x,y, 30, 33, tiburonS, 3, anim2, 0, 0 );
-  LCD_Sprite(x, y, 32, 33, ataqueMega, 3, anim2, 0, 0 );
+//SEGUNDO JOYSTICK  
+  if (potx2 <= 100) {
+    x2--;
+    if (x2 < 0) {
+      x2 = 0;
+    }
+
+  }
+  if (potx2 >= 155) {
+    x2++;
+  }
+  if (x2 > 320) {
+    x2 = 320;
+  }
+
+  if (poty2 <= 100) {
+    y2++;
+  }
+  if (y2 > 240) {
+    y2 = 240;
+  }
+
+  if (155 <= poty2) {
+    y2--;
+  }
+  if (y2 < 0) {
+    y2 = 0;
+  }
+
+  anim2 = (x2) % 3;
+
+
+  LCD_Sprite(x2,y2, 30, 33, tiburonS, 3, anim2, 0, 0 );
+  LCD_Sprite(x, y, 32, 33, ataqueMega, 3, anim1, 0, 0 );
   delay(15);
+}
+
+
+//funcion para hacer el split de la lectura serial
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length()-1;
+
+  for(int i=0; i<=maxIndex && found<=index; i++){
+    if(data.charAt(i)==separator || i==maxIndex){
+        found++;
+        strIndex[0] = strIndex[1]+1;
+        strIndex[1] = (i == maxIndex) ? i+1 : i;
+    }
+  }
+
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
